30585da135ca2da0c0901fb4cd669374f8a197d4
Caio M. S. <caiocpe@gmail.com>
2024-09-10 09:39:59 -0300

    implementando validação já com a primeira 'feature'

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,35 @@
 import fastify from "fastify";
+import { createGoal } from "../functions/create-goal";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 async function BOOT() {
+	app.post("/goals", {
+		schema: {
+			body: z.object({
+				title: z.string(),
+				desiredWeeklyFrequency: z.number().int().min(1).max(7),
+			})
+		}
+	}, async (request) => {
+		const {desiredWeeklyFrequency, title} = request.body;
+		const res = await createGoal({
+			title,
+			desiredWeeklyFrequency,
+		});
+
+		return res;
+	});
+
 	await app.listen({
 		port: 3333,
 	});
+
 
 	console.log("HTTP Server Running");
 }
