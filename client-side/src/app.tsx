44a579c85b3605d9ba477095ca1fee49d6b1ed17
Caio <caiocpe@gmail.com>
2024-09-13 20:12:26 -0300

    construindo a interface

--- client-side/src/app.tsx
+++ client-side/src/app.tsx
@@ -1,7 +1,18 @@
+import {
+	Dialog
+} from "./components/ui/dialog";
+import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
+import { Summary } from "./components/summary";
+
 export function App() {
 	return (
 		<>
-			<h1>Olá</h1>
+			<Dialog>
+				{/* <EmptyGoals />
+				<CreateGoal /> */}
+				<Summary />
+			</Dialog>
 		</>
 	);
 }
